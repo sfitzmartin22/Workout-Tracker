bc0e8c6c6ab5f59bac67855b34a6a993b9f567ea
Sean Fitzmartin <sfitzmartin22@gmail.com>
2021-05-22 14:21:39 -0400

    updating seed to seed in heroku

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 const Workout = require('../models/workout.js');
 
-mongoose.connect('mongodb://localhost/workout', {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
+  useFindAndModify: false
 });
 
 const workoutSeed = [
